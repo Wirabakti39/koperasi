151e087a0994be08583f9bc483050714c9e86fce
Dimas <94775622+Wirabakti39@users.noreply.github.com>
2022-03-21 12:26:16 +0800

    Update koperasi.py
    Fix error when inputing value and repair something.

--- koperasi.py
+++ koperasi.py
@@ -18,83 +18,90 @@ while True :
 	harga_sementara = 0
 	kode = input("\nPilih nomor dari barang yang ingin anda beli : ")
 	if kode == "1" :
-		kode = "buku tulis"
+		barang = "buku tulis"
 		nilai = "Rp. 5,000"
 		harga = 5000
 	elif kode == "2" :
-		kode = "buku kotak"
+		barang = "buku kotak"
 		nilai = "Rp. 5,000"
 		harga = 5000
 	elif kode == "3" :
-		kode = "buku gambar A3"
+		barang = "buku gambar A3"
 		nilai = "Rp. 13,000"
 		harga = 13000
 	elif kode == "4" :
-		kode = "pulpen"
+		barang = "pulpen"
 		nilai = "Rp. 3,000"
 		harga = 3000
 	elif kode == "5" :
-		kode = "spidol"
+		barang = "spidol"
 		nilai = "Rp. 7,000"
 		harga = 7000	
 	elif kode == "6" :
-		kode = "pensil"
+		barang = "pensil"
 		nilai = "Rp. 2,500"
 		harga = 2500
 	elif kode == "7" :
-		kode = "penghapus"
+		barang = "penghapus"
 		nilai = "Rp. 2,000"
 		harga = 2000
 	elif kode == "8" :
-		kode = "penggaris"
+		barang = "penggaris"
 		nilai = "Rp. 4,500"
 		harga = 4500
 	elif kode == "9" :
-		kode = "sharpener"
+		barang = "sharpener"
 		nilai = "Rp. 3,500"
 		harga = 3500
 	elif kode == "10" :
-		kode = "tip-x"
+		barang = "tip-x"
 		nilai = "Rp. 4,000"
 		harga = 4000
-	else : print("\tBarang tidak ditemukan di toko kami.")
+	else : print("\nBarang tidak ditemukan di toko kami.")
 	
-	jumlah = int(input(f"Anda ingin membeli {kode} sebanyak? : "))
-	if jumlah > 0 :	
-		for i in range(jumlah) :
-			list_barang.append(kode)
-			list_harga.append(nilai)
-			harga_sementara = harga*jumlah
+	if kode=='1' or kode=='2' or kode=='3' or kode=='4' or kode=='5' or kode=='6' or kode=='7' or kode=='8' or kode=='9' or kode=='10':
+		try :
+			jumlah = int(input(f"\n\tanda ingin membeli {barang} sebanyak? : "))
+			if jumlah > 0 :	
+				for i in range(jumlah) :
+					list_barang.append(barang)
+					list_harga.append(nilai)
+					harga_sementara = harga*jumlah
+			else :
+				break
+			total += harga_sementara
+			print("\nAda yang ingin dibeli lagi?")
+			lagi = input("\n\tketik 1 untuk memilih lagi : ")
+			if lagi != '1': break
+		except ValueError :
+			print("\nYang anda masukan bukan angka.")
+			lagi = input("\tketik 1 untuk membeli ulang : ")
+			if lagi != '1': break
+
 	else :
-		break
-	total += harga_sementara
-	print("ada yang ingin dibeli lagi?")
-	lagi = input("Ketik 'y' / 'yes' untuk memilih lagi : ")
-	if lagi == "y" or lagi == "yes" :
-		continue
-	else :
-		break
+		lagi = input("\n\tketik 1 untuk memilih ulang : ")
+		if lagi != '1': break
 
 if list_barang == [] :
-	print("\n\tAnda tidak memilih barang apapun.")
+	print("\nAnda tidak memilih barang apapun.")
 else :
 	if len(list_barang) > 4 :
 		diskon = total * 0.05
 		total -= diskon
-		print("\ndiskon 5%! karena melakukan 5 atau lebih pembelian barang.\nPotongan harga senilai Rp.", diskon)
+		print("\nDiskon 5%! karena melakukan 5 atau lebih pembelian barang.\nPotongan harga senilai Rp.", diskon)
 	print("\nBarang yang anda beli : ↓")
 	print(list_barang, "\n")
 	print("Harga semua barang yang dibeli :↓")
 	print(list_harga, "\n")
 	print(f"total harga yang harus dibayar : Rp. {total}")
 	
-	uang = int(input("Masukan pembayaran anda : Rp. "))
+	uang = int(input("\n\tmasukan pembayaran anda : Rp. "))
 	if uang > total :
-		print("\n\tAnda memiliki kembalian Rp.", uang - total)
+		print("\nAnda memiliki kembalian Rp.", uang - total)
 	elif uang == total :
-		print("\n\tAnda memberikan uang pas.")
+		print("\nAnda memberikan uang pas.")
 	else :
-		print("\n\tUang anda kurang Rp.", total - uang, " !!")
+		print("\nUang anda kurang Rp.", total - uang)
 		print("Sebagai gantinya push up 20×, Itu akan melunasi hutang anda.")
 	
 print("\nTerimakasih telah berkunjung,\nCeritakan pengalaman anda tentang toko Dimaz.")
